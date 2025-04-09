be0620e060267203e2cf43d343244546f840f7a5
Soupiershoe7 <51244472+Soupiershoe7@users.noreply.github.com>
2025-04-08 21:45:09 -0500

    Created a zooma package

--- main.py
+++
@@ -1,1 +0,0 @@
-# I don't have an outline for the code yet because I just changed my project idea
---
+++ zooma/main.py
@@ -0,0 +1,5 @@
+def main():
+    print("Welcome to Zooma! This is a Zuma clone game.")
+
+if __name__ == "__main__":
+    main()
