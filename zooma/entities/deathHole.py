ed9b592a49097432bbcaa38736060ec4661e8ecd
Soupiershoe7 <51244472+Soupiershoe7@users.noreply.github.com>
2025-05-13 23:13:13 -0500

    emitter and death hole

---
+++ zooma/entities/deathHole.py
@@ -0,0 +1,22 @@
+from pygame import Vector2
+import pygame
+from zooma.entities.entity import Entity
+from zooma.entities.chain import Chain
+
+
+class DeathHole(Entity):
+    def __init__(self, position: Vector2):
+        super().__init__()
+        self.position = Vector2(position)
+        self.death_radius = 5
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 0, 0), self.position, 20)
+        pygame.draw.circle(screen, (0, 0, 0), self.position, 18)
+        pygame.draw.circle(screen, (255, 0, 0), self.position, self.death_radius)
+
+
+    def check_collision(self, other: Chain):
+        first_ball = other.get_first_ball()
+        distance = self.position.distance_to(first_ball.position)
+        return distance < self.death_radius
---
+++ zooma/entities/emitter.py
@@ -0,0 +1,26 @@
+from pygame import Vector2, Color
+import pygame
+
+from zooma.entities.entity import Entity
+from zooma.entities.chain import Chain
+from zooma.utils.colors import LevelColors
+
+class Emitter (Entity):
+    def __init__(self, position: Vector2, level_colors: LevelColors):
+        super().__init__()
+        self.position = Vector2(position)
+        self.level_colors = level_colors
+        self.color_generator = self.level_colors.get_color_generator()
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 255, 255), self.position, 20)
+
+    def get_color(self):
+        return next(self.color_generator)
+        
+    def check_collision(self, other: Chain):
+        last_ball = other.get_last_ball()
+        distance = self.position.distance_to(last_ball.position)
+        return distance < last_ball.radius * 2 - 5
+        
+        
